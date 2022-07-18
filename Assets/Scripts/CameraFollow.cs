d760d4fdf3a832b0926e91fd0196d293605754d9
HaNgocHieu0301 <hieuhn0301@gmail.com>
2022-07-18 16:37:29 +0700

    update
    camera following, create enemy animation

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,18 +1,35 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Transform player;
+    private Vector3 tempPos;
+    [SerializeField]
+    private float minX, maxX;//dùng để hạn chế khi người chơi di chuyển đến hết bg
+
     void Start()
     {
+        player = GameObject.FindWithTag("Player").transform;
         
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        tempPos = transform.position; //vị trí hiện tại của máy ảnh x,y,z -> lưu trữ vị trí hiện tại của cam
+        tempPos.x = player.position.x; //set x của cam = toa do x cua player
+        if(tempPos.x < minX)
+        {
+            tempPos.x = minX;
+        }
+        if (tempPos.x > maxX)
+        {
+            tempPos.x = maxX;
+        }
+        //khi camera di chuyển đến các giới hạn thì k di chuyển theo nhân vật nữa
+        transform.position = tempPos; //gán ngược lại vị trí ban đầu của camera, lúc này cam sẽ có vị trí y,z như ban đầu và vị trí x lúc này đã đc lấy theo người chơi
     }
+    //chuyển thành LateUpdate để được gọi sau cùng sau update, fixedupdate
 }
---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    [HideInInspector]
+    public float speed; //ẩn với inspector tab nhưng có thể thay đổi truy cập bằng các script khác
+    private Rigidbody2D myBody;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        speed = 7;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        myBody.velocity = new Vector2(speed, myBody.velocity.y); 
+        //vận tốc tuyến tính tính theo đơn vị mỗi giây 
+        //thêm speed value vào x velocity cái sẽ đẩy player sang trái và phải
+        //k thay đổi y do di chuyển trên một đường thằng 
+        //-> move enemy
+    }
+}
